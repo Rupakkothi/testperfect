a25555b2e7ef2655a7ecbdae5c48cda5504e2903
krishna <krishna@gmail.com>
2026-06-25 19:05:12 +0530

    chore: enable network host listening in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Listen on all network interfaces (needed for mobile/local network access)
+    port: 5173
+  }
 })
+
